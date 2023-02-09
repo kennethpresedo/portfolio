e4a460bc5ba5df9a9e7f3d983aa81de31b39542e
Kenneth Presedo <kennethpresedo@live.com>
2023-02-08 19:57:12 -0500

    nav bar and footer

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,17 +1,19 @@
 import styles from './Nav.module.scss';
-
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function Nav() {
     return (
         <main className={styles.Nav}>
-            <div className={styles.navList}>
-                <a className={styles.link} href="/">Home</a> 
-                <a className={styles.link} href="/about">About Me</a> 
-                <a className={styles.link} href="/contact">Contact Me</a> 
-                <a className={styles.link} href="/portfolio">My Portfolio</a>
-            </div>
+            <span className={styles.logo}>
+                <span className={styles.KP}>Kenneth Presedo,</span>
+                <span className={styles.FSSE}>Full Stack Software Engineer</span>
+            </span>
+            <aside className={styles.navList}>
+                <a className={styles.link} href="/">Home</a> &nbsp;
+                <a className={styles.link} href="/about">About Me</a> &nbsp;
+                <a className={styles.link} href="/contact">Contact Me</a> &nbsp;
+                <a className={styles.link} href="/portfolio">My Portfolio</a> &nbsp;
+            </aside>
         </main>
     )
 }
