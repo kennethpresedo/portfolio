2c59c79ef39733de6eed630eb1a6756be24f7809
Kenneth Presedo <kennethpresedo@live.com>
2023-02-10 17:32:15 -0500

    nav and footer complete

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,17 +4,19 @@ import styles from './Footer.module.scss'
 export default function Footer() {
     return (
         <main className={styles.Footer}>
-            {/* <h1>Footer!</h1><br/> */}
-            <div className={styles.footList}>
-                <a className={styles.link} href="/">Homepage</a> 
-                <a className={styles.link} href="/about">About Me</a> 
-                <a className={styles.link} href="/contact">Contact</a> 
-                <a className={styles.link} href="/portfolio">Portfolio</a>
-            </div>
-            <div className={styles.socials}>
-                <a className={styles.socialsLink} href="https://www.linkedin.com/in/kenneth-presedo/">Linkedin</a> 
-                <a className={styles.socialsLink} href="https://www.github.com/kennethpresedo">Github</a> 
-                <a className={styles.socialsLink} href="mailto:kennethpresedo@live.com?subject=Just saw your portfolio!&body=Just wanted to let you know that, YOU ROCK!">Email Me</a>
+            <span className={styles.info}>Latino in Tech <br />Made with ♥️<br />From Miami, Fl<br />© 2023</span>
+            <div className={styles.all}>
+                <div className={styles.footList}>
+                    <a className={styles.link} href="/">Homepage</a>
+                    <a className={styles.link} href="/about">About Me</a>
+                    <a className={styles.link} href="/contact">Contact</a>
+                    <a className={styles.link} href="/portfolio">Portfolio</a>
+                </div>
+                <div className={styles.socials}>
+                    <a className={styles.socialsLink} href="https://www.linkedin.com/in/kenneth-presedo/">Linkedin</a>
+                    <a className={styles.socialsLink} href="https://www.github.com/kennethpresedo">Github</a>
+                    <a className={styles.socialsLink} href="mailto:kennethpresedo@live.com?subject=Just saw your portfolio!&body=Just wanted to let you know that, YOU ROCK!">Email Me</a>
+                </div>
             </div>
         </main>
     )
